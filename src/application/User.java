cd94bc724819eb217c2cf74110486049a0186288
Chirag Wadhwa <70256225+blackbeardth@users.noreply.github.com>
2023-04-09 15:21:32 +0530

    Update User.java

--- src/application/User.java
+++ src/application/User.java
@@ -1,6 +1,8 @@
 package application;
 
 import java.io.Serializable;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
@@ -11,7 +13,7 @@ public class User implements Serializable{
 	private static final long serialVersionUID = 3636132678451544076L;
 	private static final int PASSWORD_MIN_LENGTH = 8;
 	private String username;
-	private String password;
+	private String passwordHash;
 	private int id;
 	private int totalHoursFocused;
 	private boolean keepLoggedIn;
@@ -35,7 +37,7 @@ public class User implements Serializable{
 					
 					// Gets the user data from HashMap
 					this.username = username;
-					this.password = existingUser.getPassword();
+					this.passwordHash = existingUser.getPasswordHash();
 					this.id = existingUser.getId();
 					this.keepLoggedIn = existingUser.getKeepLoggedIn();
 					this.totalHoursFocused = existingUser.getTotalHoursFocused();
@@ -57,7 +59,7 @@ public class User implements Serializable{
 	// To be used whilst registering a user
 	public User(String username, String password, Integer id) {
 		this.username = username;
-		this.password = password;
+		this.passwordHash = User.generatePasswordHash(password);
 		this.id = id;
 		this.keepLoggedIn = false;
 		this.totalHoursFocused = 0;
@@ -80,8 +82,8 @@ public class User implements Serializable{
 		return this.username;
 	}
 	
-	public String getPassword() {
-		return this.password;
+	public String getPasswordHash() {
+		return this.passwordHash;
 	}
 
 	public boolean getKeepLoggedIn() {
@@ -100,9 +102,28 @@ public class User implements Serializable{
 		return this.hoursFocusedMap;
 	}
 
-	public boolean passwordsMatch(String password) {
-		return (this.password.equals(password));
+	public boolean passwordsMatch(String inputPassword) {
+		String inputPasswordHash = generatePasswordHash(inputPassword);
+
+        return inputPasswordHash.equals(this.passwordHash);
 	}
+	
+	private static String generatePasswordHash(String password) {
+        String passwordHash = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : messageDigest) {
+                sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+            passwordHash = sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return passwordHash;
+    }
+
 
 	public static boolean usernameAvailable(String username) {
 		try {
@@ -140,6 +161,6 @@ public class User implements Serializable{
 	
 	@Override
 	public String toString() {
-		return ("Username: " + username + ", Password: " + password + ", ID: " + id + ", totalHoursFocused: " + this.totalHoursFocused);
+		return ("Username: " + username + ", PasswordHash: " + passwordHash + ", ID: " + id + ", totalHoursFocused: " + this.totalHoursFocused);
 	}
 }
